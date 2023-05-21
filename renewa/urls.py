3ebb25a84021711c0670a8a89514b5f6b9080aec
pan-pixel <prathamarora333@gmail.com>
2023-05-21 10:23:04 +0530

    changes

--- renewa/urls.py
+++ renewa/urls.py
@@ -24,6 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('mainsite.urls')),
+    path('vendors/',include('mainsite.urls')),
+
     path('accounts/',include('accounts.urls')),
     path('socialaccounts/', include('allauth.urls')),
 
