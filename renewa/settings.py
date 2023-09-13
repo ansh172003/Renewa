c884134fd0140c9058e30b94ba6a1555cd98ceac
Ansh Goyal <ansh172003@gmail.com>
2023-09-13 10:15:04 +0530

    Added pattern for static files

--- renewa/settings.py
+++ renewa/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,10 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "renewa/static/")
+    os.path.join(BASE_DIR, "static")
 ]
 
 
--- renewa/urls.py
+++ renewa/urls.py
@@ -20,6 +20,8 @@ from django.conf import settings
 # from froala_editor import views
 
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +33,4 @@ urlpatterns = [
 
     # path('froala_editor/',include('froala_editor.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
